53f7bd69e38b7b15a5bfa04a5f696d04bc12c565
Md. Juyel Rana <mjuyelrana@gmail.com>
2022-03-12 16:25:24 +0600

    Getting Started with MongoDB

---
+++ backend/seeder.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import colors from "colors";
+import users from "./data/users.js";
+import products from "./data/products.js";
+import User from "./models/UserModel.js";
+import Product from "./models/ProductModel.js";
+import Order from "./models/OderModel.js";
+import connectDB from "./config/db.js"
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () =>{
+    try {
+        await Order.deleteMany();
+        await Product.deleteMany();
+        await User.deleteMany();
+
+        const createdUser = await User.insertMany(users);
+
+        const adminUser = createdUser[0]._id;
+
+        const smapleProducts = products.map(product=>{
+            return {...product, user: adminUser}
+        });
+
+        await Product.insertMany(smapleProducts);
+
+        console.log(`Data imported!`.green.inverse);
+        process.exit();
+
+    } catch (error) {
+        console.error(`${error}`.red.inverse);
+        process.exit(1);
+    }
+}
+
+
+const destryData = async () =>{
+    try {
+        await Order.deleteMany();
+        await Product.deleteMany();
+        await User.deleteMany();
+
+        console.log(`Data destroyed!`.red.inverse);
+        process.exit();
+
+    } catch (error) {
+        console.error(`${error}`.red.inverse);
+        process.exit(1);
+    }
+}
+
+if(process.argv[2] === '-d'){
+    destryData();
+}else{
+    importData();
+}
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,16 @@ import express from 'express';
 
 import dotenv from 'dotenv';
 
-import products from './data/products.js';
+import colors from 'colors';
+
+import connectDB from './config/db.js';
+
+import productRoutes from './routes/ProductRoutes.js';
+import { errorHandler, notFound } from './middleware/ErrorMiddleware.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -12,24 +19,15 @@ app.get('/', (req, res)=>{
     res.send('Api is running....');
 });
 
+app.use('/api/products', productRoutes);
 
-app.get('/api/products', (req, res)=>{
-    res.json(products);
-});
+app.use(notFound);
+app.use(errorHandler);
 
-
-app.get('/api/products/:id', (req, res)=>{
-
-    const product = products.find(p=>p._id === req.params.id);
-
-    if(!product){
-        res.send('No product found!');
-    }
-
-    res.json(product);
-});
 
 const PORT = process.env.PORT || 5000;
 
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
